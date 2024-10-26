32cd65defd6f4bd4bf0efffd8d8f13b9986a6fc5
Fleege <76089935+pfleege@users.noreply.github.com>
2024-10-26 10:48:54 +0300

    ADD first module to the project
    - Modify the tsconfig.json settings to include modules (es2020)
    - Add module specification to script tag in index.html

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
-"use strict";
-console.log("Hello World");
+import Income from "./modules/income.js";
+let salary = new Income("March salary", 3000, "2021-03-31");
+console.log(salary);
---
+++ modules/income.js
@@ -0,0 +1,8 @@
+class Income {
+    constructor(description, amount, date) {
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+    }
+}
+export default Income;
