1d1af1333ad3566331e29f332450769c69cc38f2
Fleege <76089935+pfleege@users.noreply.github.com>
2024-10-27 15:52:40 +0200

    ADD expense module to project
    - Add temporary script to app.ts to test expense module

---
+++ modules/expense.js
@@ -0,0 +1,8 @@
+class Expense {
+    constructor(description, amount, dueDate) {
+        this.description = description;
+        this.amount = amount;
+        this.dueDate = dueDate;
+    }
+}
+export default Expense;
