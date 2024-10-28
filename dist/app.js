84c05f6b6d6a80545daf3c6463b2981a759287f3
Fleege <76089935+pfleege@users.noreply.github.com>
2024-10-28 10:01:06 +0200

    ADD variables to grab input values from the form
    - Add ul element to index.html in order to display list of transactions

--- dist/app.js
+++ dist/app.js
@@ -1,17 +1,28 @@
-import Income from "./modules/income.js";
-import Expense from "./modules/expense.js";
-let salaryFebruary = new Income("February salary", 3000, "2021-02-28");
+// Grab items from the DOM
+const form = document.querySelector("form");
+const transactionType = document.querySelector("#transactionType");
+const date = document.querySelector("#date");
+const transactionDetails = document.querySelector("#transactionDetails");
+const amount = document.querySelector("#amount");
+// list template instance
+const ul = document.querySelector("ul");
+console.log(form, transactionType, date, transactionDetails, amount, ul);
+export {};
+/* let salaryFebruary = new Income("February salary", 3000, "2021-02-28");
 let salaryMarch = new Income("March salary", 3000, "2021-03-31");
 let rentFebruary = new Expense("February rent", 900, "2021-02-28");
 let groceryFebruary = new Expense("February grocery", 200, "2021-02-28");
 let rentMarch = new Expense("March rent", 900, "2021-03-31");
 let groceryMarch = new Expense("March grocery", 130, "2021-03-31");
+
 let incomes = 0;
 incomes += salaryFebruary.amount;
 incomes += salaryMarch.amount;
+
 let expenses = 0;
 expenses += rentFebruary.amount;
 expenses += groceryFebruary.amount;
 expenses += rentMarch.amount;
 expenses += groceryMarch.amount;
-console.log(`Net budget status - March: ${incomes - expenses}`);
+
+console.log(`Net budget status - March: ${incomes - expenses}`); */
