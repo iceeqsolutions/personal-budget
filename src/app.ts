0546642971f78dab700527ed4a2cca4b10d39657
Fleege <76089935+pfleege@users.noreply.github.com>
2024-10-28 15:15:21 +0200

    ADD render method to the form event listener
    - Use the Transaction type to determine which module to use when
    rendering the form data. Currently the content in both income.ts and
    expense.ts are the same. However, this structure leaves the option open
    to render different content based on the transaction type in the future.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import Income from "./modules/income.js";
 import Expense from "./modules/expense.js";
+import { transactionProps } from "./interfaces/transactionProps.js";
+import { TransactionsTemplate } from "./modules/transactionsTemplate.js";
 
 // Grab items from the DOM
 const form = document.querySelector("form");
@@ -11,34 +13,33 @@ const transactionDetails = document.querySelector(
   "#transactionDetails"
 ) as HTMLTextAreaElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
-// list template instance
-const ul = document.querySelector("ul");
 
-form?.addEventListener("submit", (evt: Event) => {
+// List template instance
+const ul = document.querySelector("ul")!;
+const list = new TransactionsTemplate(ul);
+
+// Grab the input values
+form?.addEventListener("submit", (evt) => {
   evt.preventDefault();
-  console.log(
-    transactionType.value,
-    date.valueAsDate,
-    transactionDetails.value,
-    amount.valueAsNumber
-  );
+  let doc: transactionProps;
+  if (transactionType.value === "income") {
+    doc = new Income(
+      transactionDetails.value,
+      amount.valueAsNumber,
+      (date.valueAsDate = new Date())
+    );
+  } else {
+    doc = new Expense(
+      transactionDetails.value,
+      amount.valueAsNumber,
+      (date.valueAsDate = new Date())
+    );
+  }
+  list.render(doc, transactionType.value);
+  //   console.log(
+  //     transactionType.value,
+  //     date.valueAsDate,
+  //     transactionDetails.value,
+  //     amount.valueAsNumber
+  //   );
 });
-
-/* let salaryFebruary = new Income("February salary", 3000, "2021-02-28");
-let salaryMarch = new Income("March salary", 3000, "2021-03-31");
-let rentFebruary = new Expense("February rent", 900, "2021-02-28");
-let groceryFebruary = new Expense("February grocery", 200, "2021-02-28");
-let rentMarch = new Expense("March rent", 900, "2021-03-31");
-let groceryMarch = new Expense("March grocery", 130, "2021-03-31");
-
-let incomes = 0;
-incomes += salaryFebruary.amount;
-incomes += salaryMarch.amount;
-
-let expenses = 0;
-expenses += rentFebruary.amount;
-expenses += groceryFebruary.amount;
-expenses += rentMarch.amount;
-expenses += groceryMarch.amount;
-
-console.log(`Net budget status - March: ${incomes - expenses}`); */
